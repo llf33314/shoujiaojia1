dab524d68e58dc63414fa18a44fa15a185341f52
psr <gt123456>
2017-10-28 09:04:06 +0800

    初始化

---
+++ scene/src/test/java/com/gt/scene/SceneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gt.scene;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SceneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
